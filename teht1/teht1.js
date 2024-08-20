de85a53faa30bae82e40f311ebcc1dbc10bf606d
Henriette Lehtonen <Henriette.Lehtonen@student.lab.fi>
2024-08-20 17:46:25 +0300

    teht 1

--- teht1/teht1.js
+++ teht1/teht1.js
@@ -1,12 +1,14 @@
-function greeting(name) {
-  console.log(name);
-}
+'use strict';
 
-greeting();
+// KIRJOITUS HTML POHJAAN
+// huom # id target !
 
-// git add . - lisää
-// git commit -m - commit ja viesti 'first commit'
-// git push
+let celcius = prompt('Anna lämpötila celcius asteina:');
+celcius = parseInt(celcius); // muutetaan string numeroiksi
+const kelvin = celcius + 273.15;
+const fah = (celcius * 9) / 5 + 32;
 
-// filezilla
-// webdisk --> users.metropolia.fi/~henriele
+document.querySelector('#lampotila').innerHTML =
+  'Lämpötila ' + celcius + ' °C on ' + kelvin + ' °K ja ' + fah + ' °F';
+
+// npm i
